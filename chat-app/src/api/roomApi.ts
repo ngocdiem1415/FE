0a6cc5e9cb592ef02779f2c71a5cc05bb251de70
NgocDiem <22130038@st.hcmuaf.edu.vn>
2025-12-20 00:45:12 +0700

    feat(sidebar): add sidebar UI components and room api

---
+++ chat-app/src/api/roomApi.ts
@@ -0,0 +1,31 @@
+import { sendSocket } from "./socket";
+
+export const createRoom = (name: string) => {
+    sendSocket({
+        action: "onchat",
+        data: {
+            event: "CREATE_ROOM",
+            data: { name },
+        },
+    });
+};
+
+export const joinRoom = (name: string) => {
+    sendSocket({
+        action: "onchat",
+        data: {
+            event: "JOIN_ROOM",
+            data: { name },
+        },
+    });
+};
+
+export const getRoomMessages = (name: string, page = 1) => {
+    sendSocket({
+        action: "onchat",
+        data: {
+            event: "GET_ROOM_CHAT_MES",
+            data: { name , page },
+        },
+    });
+};
