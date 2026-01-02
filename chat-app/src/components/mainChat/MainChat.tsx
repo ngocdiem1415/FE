3d628832d474b55734d9957f83dd1af5abc2fd27
NgocDiem <22130038@st.hcmuaf.edu.vn>
2026-01-02 08:47:57 +0700

    feat(chat): implement user search and list components

--- chat-app/src/components/mainChat/MainChat.tsx
+++ chat-app/src/components/mainChat/MainChat.tsx
@@ -1,68 +1,61 @@
 import React from 'react';
 import './mainChat.css';
+import SearchUser from './../sidebar/SearchUser';
+import UserInfor from './../sidebar/UserInfor.tsx';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const MainChat: React.FC = () => {
     return (
-        <div className='mainChat'>
-            {/* --- TRƯỜNG HỢP: CHƯA CHỌN CUỘC TRÒ CHUYỆN --- */}
-            {/* <p className="noConversation">Hãy chọn một cuộc trò chuyện</p> */}
+        <div className='chatAppContainer' style={{ display: 'flex', height: '100vh' }}>
+            <div className="leftSide" style={{ width: '30%', borderRight: '1px solid #ddd', display: 'flex', flexDirection: 'column' }}>
+                <UserInfor />
+                <SearchUser />
 
-            {/* --- TRƯỜNG HỢP: GIAO DIỆN CHAT CHÍNH --- */}
-            <React.Fragment>
-                {/* Header: Thông tin người nhận */}
-                <div className="topChat">
-                    <div className="user">
-                        <img src="/img/avatar.jpg" alt="avatar" className="avatar" />
-                        <div className="texts">
-                            <span>Tên người dùng</span>
-                            <p>Đang hoạt động</p>
-                        </div>
-                    </div>
-                    <div className="icon">
-                        <i className="fa-solid fa-circle-info fa-xl" style={{ color: '#333333' }}></i>
-                    </div>
+                <div className="userListContainer" style={{ flex: 1, overflowY: 'auto' }}>
+                    {/* Danh sách người dùng sẽ hiện ở đây */}
                 </div>
+            </div>
 
-                {/* Nội dung tin nhắn (Center) */}
-                <div className="centerChat">
-                    {/* Tin nhắn từ người khác */}
-                    <div className="messages">
-                        <img src="/img/avatar.jpg" alt="avatar" className="avatar" />
-                        <div className="texts">
-                            <p className="content">Chào bạn, hôm nay thế nào?</p>
-                            <span>10:00</span>
+            <div className="mainChat" style={{ width: '70%', display: 'flex', flexDirection: 'column' }}>
+                <React.Fragment>
+                    {/* Header: Thông tin người nhận */}
+                    <div className="topChat">
+                        <div className="user">
+                            <img src="/img/avatar.jpg" alt="avatar" className="avatar" />
+                            <div className="texts">
+                                <span>Tên người dùng</span>
+                                <p>Đang hoạt động</p>
+                            </div>
+                        </div>
+                        <div className="icon">
+                            <i className="fa-solid fa-circle-info fa-xl" style={{ color: '#333333' }}></i>
                         </div>
                     </div>
 
-                    {/* Tin nhắn của chính mình (có class 'own') */}
-                    <div className="messages own">
-                        <div className="texts">
-                            <p className="content">Mình vẫn ổn, cảm ơn bạn!</p>
-                            <span>10:01</span>
+                    {/* Nội dung tin nhắn (Center) */}
+                    <div className="centerChat" style={{ flex: 1, overflowY: 'auto' }}>
+                        <div className="messages">
+                            <img src="/img/avatar.jpg" alt="avatar" className="avatar" />
+                            <div className="texts">
+                                <p className="content">Chào bạn, hôm nay thế nào?</p>
+                                <span>10:00</span>
+                            </div>
+                        </div>
+                        <div className="messages own">
+                            <div className="texts">
+                                <p className="content">Mình vẫn ổn, cảm ơn bạn!</p>
+                                <span>10:01</span>
+                            </div>
                         </div>
                     </div>
 
-                    {/* Điểm neo để cuộn xuống tin nhắn mới nhất */}
-                    <div></div>
-                </div>
-
-                {/* Thanh nhập liệu (Bottom) */}
-                <div className="bottomChat">
-                    <input
-                        type="text"
-                        placeholder="Nhập tin nhắn..."
-                    />
-                    <div className="emoji">
-                        <i className="fa-regular fa-face-smile fa-xl"></i>
-                        {/* Khu vực hiển thị Emoji Picker */}
-                        {/* <div className="emojiPicker">
-                            <EmojiPicker />
-                        </div> */}
+                    {/* Thanh nhập liệu (Bottom) */}
+                    <div className="bottomChat">
+                        <input type="text" placeholder="Nhập tin nhắn..." />
+                        <button className="sendButton">Gửi</button>
                     </div>
-                    <button className="sendButton">Gửi</button>
-                </div>
-            </React.Fragment>
+                </React.Fragment>
+            </div>
         </div>
     );
 };
