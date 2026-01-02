e220defd4553948e68e4c4e3d8e9fafa6b29d5ed
NgocDiem <22130038@st.hcmuaf.edu.vn>
2026-01-02 08:47:48 +0700

    feat(ui): update user types and profile sidebar UI

--- chat-app/src/components/sidebar/UserInfor.tsx
+++ chat-app/src/components/sidebar/UserInfor.tsx
@@ -1,25 +1,20 @@
-import './userAvatar.css'
+import React from "react";
+import './userInfor.css';
 
 interface UserInforProps {
     username?: string;
     isOnline: boolean;
-    isExit: boolean;
 }
 
 const UserInfor = ({
-                       username = localStorage.getItem('username') || 'Guest',
+                       username = localStorage.getItem('username'),
                        isOnline,
                        isExit
                    }: UserInforProps) => {
-
     return (
         <div className={`user-avatar ${!isExit ? 'user-not-exit' : ''}`}>
             <div className="avatar-wrapper">
-                <img
-                    src="/react.svg"
-                    alt="avatar"
-                    className={`avatar-img ${!isExit ? 'grayscale' : ''}`}
-                />
+                <img src="/img/avatar.jpg" alt="avatar" className="avatar" />
                 {/* Nếu isOnline là true thì thêm class 'online', ngược lại 'offline' */}
                 <span className={`status-dot ${isOnline ? 'online' : 'offline'}`}></span>
             </div>
