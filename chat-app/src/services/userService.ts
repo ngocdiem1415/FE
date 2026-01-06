84824d7711aa7b6c32c3433ba135e9f7cc5f8ff2
Nguyễn Hữu Thịnh <22130269@st.hcmuaf.edu.vn>
2026-01-06 12:06:19 +0700

    Fix login, register, socketClient.ts

--- chat-app/src/services/userService.ts
+++ chat-app/src/services/userService.ts
@@ -1,22 +1,23 @@
-import { sendSocket } from "../api/socketClient";
+import { sendSocketSafe } from "../api/socketClient";
 import { ACTION_NAME, ChatEvent } from "../constants/chatEvents";
 
 export const peopleService = {
   getPeopleMessages: (name: string, page = 1) =>
-    sendSocket(
-        { action: ACTION_NAME, data: 
-            { event: ChatEvent.GET_PEOPLE_CHAT_MES,
-                 data: { name, page } 
-                } }
-            ),
+    sendSocketSafe({
+      action: ACTION_NAME,
+      data: {
+        event: ChatEvent.GET_PEOPLE_CHAT_MES,
+        data: { name, page },
+      },
+    }),
 };
 
 export const userService = {
-  getUserList: () => 
-    sendSocket(
-        { action: ACTION_NAME, data: 
-            { event: 
-                ChatEvent.GET_USER_LIST 
-            } }
-        ),
+  getUserList: () =>
+    sendSocketSafe({
+      action: ACTION_NAME,
+      data: {
+        event: ChatEvent.GET_USER_LIST,
+      },
+    }),
 };
